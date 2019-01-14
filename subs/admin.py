71f8db19d4e4d85faa3128658977c029c4c8c477
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-01-14 16:04:59 -0300

    Criados models do app subs

--- subs/admin.py
+++ subs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Subrediti, Post, Thread
 
-# Register your models here.
+admin.site.register(Subrediti)
+admin.site.register(Post)
+admin.site.register(Thread)
--- subs/models.py
+++ subs/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from common.models import IndexedTimeStampedModel
+from users.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Subrediti(IndexedTimeStampedModel):
+    """ Model que define um subrediti, que agrega várias threads. """
+    name = models.CharField(max_length=50, default='New post')
+    creator = models.ForeignKey(User, related_name='subreditis',
+                                on_delete=models.CASCADE)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Thread(IndexedTimeStampedModel):
+    """ Model que define um thread, que agrega vários posts. """
+    author = models.ForeignKey(User, related_name='threads',
+                               on_delete=models.CASCADE)
+
+    title = models.TextField()
+    subrediti = models.ForeignKey(Subrediti, related_name='threads',
+                                  on_delete=models.CASCADE)
+
+    vote_count = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
+
+class Post(IndexedTimeStampedModel):
+    """ Model que define um thread, que agrega vários posts. """
+    vote_count = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name='posts',
+                               on_delete=models.CASCADE)
+
+    content = models.TextField()
+
+    thread = models.ForeignKey(Thread, related_name='posts',
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content

fe3795f29014b4bcbdd9796f817c2f822d9fbe1c
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-01-14 10:48:44 -0300

    App users registrado no painel admin

--- common/models.py
+++ common/models.py
@@ -7,6 +7,6 @@ class IndexedTimeStampedModel(models.Model):
     """ Model to register edition and creation dates """
     created = models.DateTimeField(auto_now_add=True,)
     updated = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         abstract = True
--- rediti/settings.py
+++ rediti/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'common'
-    'users',
+    'common.apps.CommonConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 

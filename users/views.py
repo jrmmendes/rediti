0f4cca3c26e1e929808809ffc4d949d778149251
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-01-16 09:45:33 -0300

    Criado formview para signup

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from .models import User
+from .forms import UserForm
 from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
@@ -26,3 +28,15 @@ class LoginView(LoginView):
             return HttpResponseRedirect(reverse('common:home'))
         return super(LoginView, self).get(request)
 
+
+class SignupView(generic.FormView):
+    success_url = 'users:login'
+    template_name = 'users/signup.html'
+    redirect_authenticated_user = True
+
+    model = get_user_model()
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('common:home'))
+        return super(LoginView, self).get(request)

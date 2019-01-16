3688fc11a5b5c3bf78707fe81418c1bcbf99d489
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-01-16 11:44:14 -0300

    Criada detailView para subreditis

--- subs/urls.py
+++ subs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SubsView
+from .views import SubsView, SubView
 
 app_name = 'subs'
 
 urlpatterns = [
     path('', SubsView.as_view(), name='all'),
+    path('<slug:slug>/', SubView.as_view(), name='r'),
 ]
--- subs/views.py
+++ subs/views.py
@@ -8,3 +8,8 @@ class SubsView(generic.ListView):
     context_object_name = 'subs'
     ordering = ['created']
     paginate_by = 10
+
+
+class SubView(generic.DetailView):
+    template_name = 'subs/subrediti.html'
+    model = Subrediti
